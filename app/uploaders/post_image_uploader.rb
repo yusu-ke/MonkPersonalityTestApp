7e0a5dfcd09b61c105dc5cf7cb81c45b3883abcb
Yusuke <yusuke.052835@gmail.com>
2024-12-25 13:24:53 +0900

    Lintの修正

--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -17,7 +17,7 @@ class PostImageUploader < CarrierWave::Uploader::Base
   def default_url(*args)
   # For Rails 3.1+ asset pipeline compatibility:
   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #    "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+    #    "/images/fallback/" + [version_name, "default.png"].compact.join('_')
     "default.png"
   end
 
