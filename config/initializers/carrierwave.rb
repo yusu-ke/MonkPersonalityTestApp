70af7af6d6a2dc643dda30ca8085b2eaa233f3f4
Yusuke <yusuke.052835@gmail.com>
2024-12-27 20:31:12 +0900

    Lint修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,10 +7,10 @@ CarrierWave.configure do |config|
     config.fog_provider = "fog/aws"
     config.fog_directory  = "monkmatch" # 作成したバケット名を記述
     config.fog_credentials = {
-      provider: 'AWS',
+      provider: "AWS",
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"], # 環境変数
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"], # 環境変数
-      region: 'ap-northeast-3',
+      region: "ap-northeast-3",
       path_style: true
     }
 
