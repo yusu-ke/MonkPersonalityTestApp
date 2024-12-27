a631c05d4401e70e434421432841e52dd8cb2fd8
Yusuke <yusuke.052835@gmail.com>
2024-12-27 21:25:39 +0900

    構文修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,12 +4,12 @@ require "carrierwave/storage/fog"
 
 CarrierWave.configure do |config|
     config.storage :fog
-    config.fog_directory  = "monkmatch" # 作成したバケット名を記述
+    config.fog_directory  = ENV['S3_BUCKET_NAME'] # 作成したバケット名を記述
     config.fog_credentials = {
       provider: "AWS",
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"], # 環境変数
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"], # 環境変数
-      region: "ap-northeast-3"
+      region: ENV['S3_REGION']
     }
 
     config.fog_public = false
