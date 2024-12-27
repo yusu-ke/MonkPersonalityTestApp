ab1ab23377d774490febaf1db83a778cc9446daf
Yusuke <yusuke.052835@gmail.com>
2024-12-27 21:13:59 +0900

    構文修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,7 @@ CarrierWave.configure do |config|
       provider: "AWS",
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"], # 環境変数
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"], # 環境変数
-      region: "ap-northeast-3",
+      region: "ap-northeast-3"
     }
 
     config.fog_public = false
