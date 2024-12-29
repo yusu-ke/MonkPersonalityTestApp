419507f7a6ec558ea3c551cc934feea479fed392
Yusuke <yusuke.052835@gmail.com>
2024-12-29 19:30:48 +0900

    add:GoogleMap

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,20 @@ class PostsController < ApplicationController
 
   def create
     @post = current_user.posts.build(post_params)
+    logger.debug "Params: #{params[:post][:map_attributes]}"
     if @post.save
+      if params[:post][:map_attributes].present?
+        latitude = params[:post][:map_attributes][:latitude]
+        longitude = params[:post][:map_attributes][:longitude]
+        unless latitude.empty? && longitude.empty?
+          @map = @post.build_map(
+            latitude: latitude,
+            longitude: longitude
+          )
+          @map.save
+        end
+      end
+
       redirect_to posts_path, success: "掲示板を作成しました。"
     else
       flash.now[:danger] = "掲示板の作成に失敗しました。"
@@ -20,6 +33,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @map = @post.map
     unless current_user.view_counts.find_by(user_id: current_user.id, post_id: @post.id)
       current_user.view_counts.create(post_id: @post.id)
     end
@@ -48,6 +62,12 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:temple_name, :location, :comment, { post_images: [] })
+    params.require(:post).permit(
+      :temple_name,
+      :location,
+      :comment,
+      { post_images: [] },
+      map_attributes: [:id, :latitude, :longitude]
+      )
   end
 end
