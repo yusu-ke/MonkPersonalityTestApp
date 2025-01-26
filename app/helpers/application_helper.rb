6cb832fff16a14ecc1d4996b04caf1cd6ac498c4
Yusuke <yusuke.052835@gmail.com>
2025-01-26 19:53:29 +0900

    動的ogp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,22 @@
 module ApplicationHelper
   def default_meta_tags
+    og_image = case @result&.name
+    when "法然上人"
+      image_url("法然ogp.png")
+    when "親鸞聖人"
+      image_url("親鸞ogp.png")
+    when "一遍上人"
+      image_url("一遍ogp.png")
+    when "日蓮聖人"
+      image_url("日蓮ogp.png")
+    when "栄西禅師"
+      image_url("栄西ogp.png")
+    when "道元禅師"
+      image_url("道元ogp.png")
+    else
+      image_url("ogp.png")
+    end
+
     {
       site: "名僧マッチ",
       title: "名僧マッチ：あなたに合う名僧を選ぶ診断サービス",
@@ -15,14 +32,14 @@ module ApplicationHelper
         description: :description,
         type: "website",
         url: request.original_url,
-        image: image_url("ogp.png"),
+        image: og_image,
         locale: "ja-JP"
       },
       twitter: {
         card: "summary_large_image",
         title: "名僧マッチ（診断×歴史の名僧）",
         description: "簡単診断であなたにぴったりな歴史的な名僧に出会うことができます。",
-        image: image_url("ogp.png")
+        image: og_image
       }
     }
   end
