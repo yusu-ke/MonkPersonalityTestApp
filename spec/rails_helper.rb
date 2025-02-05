b0ce3bd3fc7740f825c10b98e5195d2645a33eaa
Yusuke <yusuke.052835@gmail.com>
2025-02-05 13:09:38 +0900

    Selenium::WebDriver環境最適化

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -113,8 +113,9 @@ RSpec.configure do |config|
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
-    options.add_argument('--window-size=1920,1080')
-
+    options.add_argument('--window-size=1024,768')
+    options.add_argument('disable-dev-shm-usage')
+    options.add_argument('disable-extensions')
     Capybara::Selenium::Driver.new(app, browser: :chrome,  options: options, url: 'http://selenium:4444/wd/hub')
   end
 
