2bb1105d8ae6a08a193d2fc38072289ea942cfee
Yusuke <yusuke.052835@gmail.com>
2025-02-09 13:33:31 +0900

    オンザフライのコンパイル設定

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -65,6 +65,8 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
 
+  config.public_file_server.enable = true
+
   config.assets.compile = true
   config.assets.digest = true
   config.assets.precompile += %w[ application.tailwind.css *.png *.jpg *.jpeg *.gif favicon.ico ]
